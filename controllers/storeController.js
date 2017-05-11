e4544126eff4a77eac0ee6796e2850b06757a40f
gary <ggrumbley@ucsd.edu>
2017-05-11 14:24:59 -0700

    Single store info page added

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -14,15 +14,15 @@ const multerOptions = {
       next({ message: "That filetype isn't allowed." }, false);
     }
   }
-}
+};
 
 exports.homePage = (req, res) => {
   res.render('index', { title: 'Home'});
-}
+};
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
-}
+};
 
 exports.upload = multer(multerOptions).single('photo');
 
@@ -31,7 +31,7 @@ exports.resize = async (req, res, next) => {
   if (!req.file) {
     next();
     return;
-  }
+  };
 
   // Build Image File
   const extension = req.file.mimetype.split('/')[1];
@@ -44,23 +44,23 @@ exports.resize = async (req, res, next) => {
 
   // Once photo is written to FS, keepp going.
   next();
-}
+};
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
-}
+};
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
-}
+};
 
 exports.editStore = async (req, res) => {
   const store = await Store.findOne({ _id: req.params.id });
   res.render('editStore', { title: `Edit ${store.name}`, store });
-}
+};
 
 exports.updateStore = async (req, res) => {
   req.body.location.type = 'Point';
@@ -70,4 +70,11 @@ exports.updateStore = async (req, res) => {
   }).exec();
   req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
   res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  // res.json(store);
+  res.render('store', { title: store.name, store })
 };
